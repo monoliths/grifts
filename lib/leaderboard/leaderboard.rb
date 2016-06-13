adeeeda77525515522c86bb34c36c6a0e5861139
gerald hernandez <gerald2557@gmail.com>
2016-06-12 23:26:55 -0700

    redid the project from the group up to fix my mistakes. Leaderbaords is in its own class now. Will add more features as I plan out the requirements

--- lib/leaderboard/leaderboard.rb
+++ lib/leaderboard/leaderboard.rb
@@ -2,40 +2,33 @@ require "open-uri"
 require "nokogiri"
 
 class Leaderboard
-
   attr_accessor :season, :hero, :ladder
+  attr_reader :hero_classes
 
-  def initialize(season = 6, hero = 'barb')
+  def initialize(season = 6, hero = :barb)
+    @hero_classes = {:barbarian => "barbarian", :witchdoctor => "wd",
+        :demonhunter => "dh", :monk => "monk", :wizard => "wizard",
+        :crusader => "crusader", :barb => "barbarian", :wd => "wd",
+        :dh => "dh", :wiz => "wizard", :sader => "crusader"}
     @season = season.to_s
-    @hero = hero
-    @ladder = Array.new
+    @hero = hero_classes[hero]
+    @ladder = []
   end
 
   def scrape
-    url = "http://us.battle.net/d3/en/rankings/season/#{@season}/rift-#{@hero_classes[@hero]}"
+    url = "http://us.battle.net/d3/en/rankings/season/#{@season}/rift-#{@hero}"
     doc = Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
     leaders_unformated = doc.xpath('//*/table/tbody/tr//*/a/@href')
 
-    leaders.each_with_index do |player, index|
+    leaders_unformated.each_with_index do |player, index|
         @ladder << player.value
         @ladder[index].slice!("/d3/en/profile/")
         @ladder[index].slice!("/")
     end
-
+    return true
   end
 
-  private
-    @hero_classes = {
-        "barbarian" => "barbarian",
-        "witchdoctor" => "wd",
-        "demonhunter" => "dh",
-        "monk" => "monk",
-        "wizard" => "wizard",
-        "crusader" => "crusader",
-        "barb" => "barbarian",
-        "wd" => "wd",
-        "dh" => "dh",
-        "wiz" => "wizard",
-        "sader" => "crusader"
-    }.freeze
+  def print_leaderboard(range = (0..9))
+   puts @ladder[range]
+  end
 end
