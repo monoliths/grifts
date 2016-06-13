ce21fa6f494a57e4a2728236d98191cad8e0ec9f
gerald hernandez <gerald2557@gmail.com>
2016-06-12 21:41:24 -0700

    put the leaderbard and scrapping process in its own class

--- lib/grifts.rb
+++ lib/grifts.rb
@@ -1,41 +1,6 @@
 require "grifts/version"
-require 'open-uri'
-require 'nokogiri'
-
-$classes = {
-    "barbarian" => "barbarian",
-    "witchdoctor" => "wd",
-    "demonhunter" => "dh",
-    "monk" => "monk",
-    "wizard" => "wizard",
-    "crusader" => "crusader",
-    "barb" => "barbarian",
-    "wd" => "wd",
-    "dh" => "dh",
-    "wiz" => "wizard",
-    "sader" => "crusader"
-}
+require "leaderboard/leaderboard"
 
 module Grifts
   extend self
-
-  def leaderboard(selected_class, top_positions, season)
-    top_positions = top_positions || 25
-    season = season || '6'
-
-    url = "http://us.battle.net/d3/en/rankings/season/#{season}/rift-#{$classes[selected_class]}"
-    doc = Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
-
-    leaders = doc.xpath('//*/table/tbody/tr//*/a/@href')[0..top_positions]
-    ladder = Array.new
-
-    leaders.each_with_index do |player, index|
-        ladder << player.value
-        ladder[index].slice!("/d3/en/profile/")
-        ladder[index].slice!("/")
-    end
-
-    puts ladder
-  end
-
 end
---
+++ lib/grifts/leaderboard/leaderboard.rb
@@ -0,0 +1,41 @@
+require "open-uri"
+require "nokogiri"
+
+class Leaderboard
+
+  attr_accessor :season, :hero, :ladder
+
+  def initialize(season = 6, hero = 'barb')
+    @season = season.to_s
+    @hero = hero
+    @ladder = Array.new
+  end
+
+  def scrape
+    url = "http://us.battle.net/d3/en/rankings/season/#{@season}/rift-#{@hero_classes[@hero]}"
+    doc = Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
+    leaders_unformated = doc.xpath('//*/table/tbody/tr//*/a/@href')
+
+    leaders.each_with_index do |player, index|
+        @ladder << player.value
+        @ladder[index].slice!("/d3/en/profile/")
+        @ladder[index].slice!("/")
+    end
+
+  end
+
+  private
+    @hero_classes = {
+        "barbarian" => "barbarian",
+        "witchdoctor" => "wd",
+        "demonhunter" => "dh",
+        "monk" => "monk",
+        "wizard" => "wizard",
+        "crusader" => "crusader",
+        "barb" => "barbarian",
+        "wd" => "wd",
+        "dh" => "dh",
+        "wiz" => "wizard",
+        "sader" => "crusader"
+    }.freeze
+end
